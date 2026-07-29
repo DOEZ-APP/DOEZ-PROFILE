e29f13bd2aa6140f36d0745b54975b7f389f05d9
DOEZ-APP <mkb00515@gmail.com>
2026-07-30 06:01:15 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXI7AsN_zJmWN13-coXPSd82QnvSktt3Q",
+  authDomain: "doezprofile.firebaseapp.com",
+  projectId: "doezprofile",
+  storageBucket: "doezprofile.firebasestorage.app",
+  messagingSenderId: "1080169571941",
+  appId: "1:1080169571941:web:66b46c198cdf6610d1c88c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
